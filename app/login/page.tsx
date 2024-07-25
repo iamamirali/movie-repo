3d2d8587177846dc128a9761e5fa5369c03ac6a3
AmirAli Shabani <iamamirali.sh@gmail.com>
2024-07-25 19:13:19 +0330

    feat: initialize login page

---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+export default function Login(): JSX.Element {
+  return (
+    <main>
+      <img src="/asset/images/login/tape-small.svg" alt="tape" />
+      <h1>hello world</h1>
+      <h2>Enter your details in order to sign in</h2>
+    </main>
+  );
+}
