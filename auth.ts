c96afd4e44d495ddfdb987d513f51c4e4cf4b30b
AmirAli Shabani <iamamirali.sh@gmail.com>
2024-09-22 11:54:23 +0330

    chore: comment deletion

--- auth.ts
+++ auth.ts
@@ -20,7 +20,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         user = await findUser(credentials);
         if (!user) {
           // No user found, so this is their first attempt to login
-          // meaning this is also the place you could do registration
           throw new Error("User not found.");
         }
         // return user object with their profile data
