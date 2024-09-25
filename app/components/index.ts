126445e5f8355afc812b65eca8c18ab9ab2af7e4
AmirAli Shabani <iamamirali.sh@gmail.com>
2024-09-25 12:45:16 +0330

    feat: add react-icons package & create Header

---
+++ app/components/index.ts
@@ -0,0 +1,2 @@
+export { Header } from "./layout";
+export { IconButton } from "./button";
--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,13 @@
 // import Link from "next/link";
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
+import { Header } from "./components";
 
 export default async function Home() {
   const session = await auth();
   if (!session?.user) redirect("/login");
 
-  return <h2>hello</h2>;
+  return <Header />;
   // redirect("/login");
   // return (
   //   <main className="flex min-h-screen flex-col items-center justify-between p-24">
