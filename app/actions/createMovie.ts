1151d9930bf6b4ba4a65885628efd1b55beacb5d
AmirAli Shabani <iamamirali.sh@gmail.com>
2024-11-26 14:39:35 +0330

    chore: a comment

--- app/actions/createMovie.ts
+++ app/actions/createMovie.ts
@@ -5,6 +5,7 @@ import cloudinary from '@/lib/cloudinary';
 import { prisma } from '@/lib/db';
 import { revalidateTag } from 'next/cache';
 
+// init state
 type TInitialState = {
   message: string;
 };
